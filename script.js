9bb17928bdd502df3053d223fa2b7ac623a4af4a
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-01-07 12:57:19 -0700

    Display markers when clicked and switch active players

--- script.js
+++ script.js
@@ -4,11 +4,44 @@ const createPlayer = (name, marker) => {
 };
 
 // gameboard object
-const gameBoard = () => {
-  let board = ["", "", "", "", "", "", "", "", ""];
+const gameBoard = (() => {
+  let boardArray = ["", "", "", "", "", "", "", "", ""];
+
+  let board = document.querySelector("#gameboard");
+  boardArray.forEach((item, index) => {
+    const square = document.createElement("div");
+    square.className = "square";
+    board.appendChild(square);
+  });
+
+  Array.from(board.children).forEach((square, index) => {
+    square.addEventListener("click", () => {
+      square.classList.add(game.activePlayer.marker);
+      square.setAttribute("data", game.activePlayer.marker);
+      board[index] = game.activePlayer.marker;
+      square.style.pointerEvents = "none";
+      game.nextPlayer();
+    });
+  });
   return {
-    board,
+    boardArray,
   };
-};
+})();
 
 //game object
+const game = (() => {
+  const playerOne = createPlayer("Player 1", "x");
+  const playerTwo = createPlayer("Player 2", "o");
+  let activePlayer = playerOne;
+
+  function nextPlayer() {
+    this.activePlayer === playerOne
+      ? (this.activePlayer = playerTwo)
+      : (this.activePlayer = playerOne);
+  }
+
+  return {
+    activePlayer,
+    nextPlayer,
+  };
+})();
